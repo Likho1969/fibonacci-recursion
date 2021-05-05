1ad051fba367b7fdc897326746671c573d33bb96
Likho1969 <likhokapesi04@gmail.com>
2021-05-05 16:43:23 +0200

    gui calculator

--- recursion-fibonacci.py
+++ recursion-fibonacci.py
@@ -1,6 +1,7 @@
 
 # Python program to display the Fibonacci sequence
 
+# defining a function
 def recur_fibo(n):
    if n <= 1:
        return n
@@ -8,12 +9,14 @@ def recur_fibo(n):
        return(recur_fibo(n - 1) + recur_fibo(n -2 ))
 
 
-n = int(input("Enter any number: "))
+n = int(input("Enter any number: "))     # asking/ prompting the user to input a figure of choice
 
 # check if the number of terms is valid
 if n <= 0:
    print("Plese enter a positive integer")
 else:
    print("Fibonacci sequence:")
-   for i in range(n):
-       print(recur_fibo(i))
+
+# looping through the n range
+for i in range(n):
+    print(recur_fibo(i))
